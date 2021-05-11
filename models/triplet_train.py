3a9a7c597af11a04c984422c74dca6b29a25ca61
root <root@instance-1.us-west1-b.c.keous-285923.internal>
2021-05-11 02:35:31 +0000

    bug in triplet

--- models/triplet_train.py
+++ models/triplet_train.py
@@ -4,4 +4,4 @@ import os
 os.system('gsutil cp gs://keous-model-files/data/{} .'.format('big_collection.msgpack'))
 model = MyModel().fresh_load()
 c=Collection().load('big_collection.msgpack')
-model.triplet_train_collection(c,epochs=1,batch_size=3, headline_emb=True, 'paper_triplet_title_{}.pt')
+model.triplet_train_collection(c,epochs=1,batch_size=3, headline_emb=True, save='paper_triplet_title_{}.pt')
